8944610c06711f82e91676be7a902253e329e932
Sara Crowbridge <sara.crowbridge@hotmail.com>
2021-03-04 09:12:44 -0600

    added files

---
+++ packages/my-first-theme/src/index.js
@@ -0,0 +1,20 @@
+const Root = () => {
+  return (
+    <>
+      <h1>>Hello Frontity</h1>
+    </>
+  );
+};
+
+export default {
+  name: "my-first-theme",
+  roots: {
+    theme: Root
+  },
+  state: {
+    theme: {}
+  },
+  actions: {
+    theme: {}
+  }
+};
